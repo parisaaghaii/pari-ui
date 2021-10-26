8b642ba8a6f4e7bf93d11df4de6cfa8b03355769
parisa aghaii <parisaa.aghaii@gmail.com>
2021-10-26 12:54:30 +0330

    input designer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import { Button } from "./lib";
+import { Button , Input } from "./lib";
 import "./App.css";
+
 function App() {
   return (
     <div className="page">
       <h3>This is my new project</h3>
       <Button label="test" kind="primary" />
+      <Input type="text"/>
     </div>
   );
 }
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,2 +1,4 @@
 import Button from "./components/Button";
-export { Button };
+import Input from "./components/Input";
+
+export { Button , Input};
