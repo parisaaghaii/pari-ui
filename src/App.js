56cb20a0d0f0fe089174f35283a09d914f6b8758
parisa aghaii <parisaa.aghaii@gmail.com>
2021-10-26 10:54:10 +0330

    base project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Button } from "./lib";
+import "./App.css";
+function App() {
+  return (
+    <div className="page">
+      <h3>This is my new project</h3>
+      <Button label="test" kind="primary" />
+    </div>
+  );
+}
+export default App;
